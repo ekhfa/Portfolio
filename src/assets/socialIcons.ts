beb84236497b397099f0c36e38c8f042f0c363cf
Ekhfa Hossain <ekhfahossain@gmail.com>
2023-10-12 21:57:21 +0600

    Fixed Design

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -4,7 +4,6 @@ const socialIcons: SocialIcons = {
   Github: `<svg 
   xmlns="http://www.w3.org/2000/svg" 
   class="icon icon-tabler icon-tabler-brand-github" 
-  width="64" height="64" 
   stroke-linecap="round" 
   stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -16,7 +15,7 @@ const socialIcons: SocialIcons = {
     class="icon-tabler"
     stroke-linecap="round"
     stroke-linejoin="round"
-    width="64" height="64" 
+    
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <path
@@ -28,8 +27,7 @@ const socialIcons: SocialIcons = {
     class="icon-tabler"
     stroke-linecap="round"
     stroke-linejoin="round"
-    width="64" height="64" 
-   
+    
     >
     <path d="M2 7v1l11 4 9-4V7L11 4z"></path>
     <path d="M4 11v4.267c0 1.621 4.001 3.893 9 3.734 4-.126 6.586-1.972 7-3.467.024-.089.037-.178.037-.268V11L13 14l-5-1.667v3.213l-1-.364V12l-3-1z"></path>
@@ -49,7 +47,7 @@ const socialIcons: SocialIcons = {
   LinkedIn: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
-    width="64" height="64" 
+    
     stroke-linecap="round"
     stroke-linejoin="round"
   >
@@ -63,7 +61,6 @@ const socialIcons: SocialIcons = {
   Mail: `<svg
       xmlns="http://www.w3.org/2000/svg"
       class="icon-tabler"
-      width="64" height="64" 
       stroke-linecap="round"
       stroke-linejoin="round"
 
