a49540ff244dca27ba9d014a2dfe8ecf68a8972f
Ekhfa Hossain <ekhfahossain@gmail.com>
2023-11-09 00:45:37 +0600

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { Image } from "astro:assets";
 
 // https://astro.build/config
 export default defineConfig({
